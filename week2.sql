d4037acc571b4496d2af9bd97d8538ac8aeaf59a
high-east <rhehd127@gmail.com>
2021-11-19 21:19:16 +0900

    Add week2.sql

---
+++ week2.sql
@@ -0,0 +1,7 @@
+-- Count MAU (Monthly Active User)
+SELECT to_char(ts.ts, 'YYYY-MM'), COUNT(usc.userID) mau
+FROM raw_data.session_timestamp ts
+JOIN raw_data.user_session_channel usc ON ts.sessionID = usc.sessionID
+GROUP BY 1
+ORDER BY 1
+LIMIT 5
