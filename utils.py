cd485c5ab48a2bb912072a4b26a0b428772b0204
High-East <rhehd127@gmail.com>
2022-01-04 12:11:51 +0900

    [ADD] Color

--- utils.py
+++ utils.py
@@ -26,3 +26,35 @@ def get_redshift_connection():
     ))
     conn.set_session(autocommit=True)
     return conn.cursor()
+
+
+class Color:
+    # print(Color.BOLD + 'Hello World !' + Color.END)
+    RED = '\033[91m'
+    YELLOW = '\033[93m'
+    GREEN = '\033[92m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    PURPLE = '\033[95m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+    @staticmethod
+    def show():
+        print(
+            'Color(',
+            f'    {Color.RED}RED{Color.END}',
+            f'    {Color.YELLOW}YELLOW{Color.END}',
+            f'    {Color.GREEN}GREEN{Color.END}',
+            f'    {Color.BLUE}BLUE{Color.END}',
+            f'    {Color.CYAN}CYAN{Color.END}',
+            f'    {Color.DARKCYAN}DARKCYAN{Color.END}',
+            f'    {Color.PURPLE}PURPLE{Color.END}',
+            f'    {Color.BOLD}BOLD{Color.END}',
+            f'    {Color.UNDERLINE}UNDERLINE{Color.END}',
+            f'    {Color.END}END{Color.END}',
+            ')',
+            sep='\n'
+        )
